d88e2b1c4bb46250bafe1f15d9409821b6e1b04e
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-07-16 13:43:01 +0300

    Add files via upload

---
+++ Java Multithreading 2313 Arkanoid2/src/com/company/Arkanoid.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Arkanoid {
+    public static void main(String[] args) {
+        
+    }
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
